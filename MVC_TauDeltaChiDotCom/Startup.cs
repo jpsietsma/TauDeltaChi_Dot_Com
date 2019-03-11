f3036ae57bfa52be27fbb4605729f000eef28d57
jpsietsma <jpsietsma@gmail.com>
2019-03-11 01:24:08 -0400

    Add project files.

---
+++ MVC_TauDeltaChiDotCom/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_TauDeltaChiDotCom.Startup))]
+namespace MVC_TauDeltaChiDotCom
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
